b0d592bbd21e99aec1d38587ee34d3a521a2fce0
acreatorwithvision <fake49403@gmail.com>
2025-08-09 00:46:56 +0530

    Modified ArrayList collection

--- src/DasaraPlannerApp.java
+++ src/DasaraPlannerApp.java
@@ -4,27 +4,47 @@ import java.util.ArrayList;
 
 public class DasaraPlannerApp{
     public static void main(String[] args){
-        //create Event object
-        Event jumbooSavari = new Event();
-        jumbooSavari.eventName="Jumbo Savari(Procession)";
-        jumbooSavari.venue="Mysore Palace to Bannimantap";
-        jumbooSavari.eventDate="15-10-2025";
+        System.out.println("--- Mysuru Dasara 2025 Event Planner ---");
 
-        //create visitor object to add a person
-        Visitor firstVisitor=new Visitor();
-        firstVisitor.visitorName="Suhas G";
-        firstVisitor.phoneNumber="8928734644";
+        //Initialize ArrayList for Event class to hold its objects as collections
+        ArrayList<Event> events=new ArrayList<>();
 
-        //Print all the details to the console
-        System.out.println("--Mysore Dasara Event Planner 2025--");
-        System.out.println("\nFeatured Event Details: ");
-        System.out.println("Event Name: "+jumbooSavari.eventName);
-        System.out.println("Event Venue: "+jumbooSavari.venue);
-        System.out.println("Event Date: "+jumbooSavari.eventDate);
+        //Add events to Event Object.
+        Event event1 = new Event();
+        event1.eventName="Jumbo Savari(Procession)";
+        event1.venue="Mysuru Palace to Bannimantapa";
+        event1.eventDate="15-10-2025";
+        events.add(event1); //Push event1 object to events collection
 
-        //Print registered users to the console
-        System.out.println("\nRegistered Visitor Details");
-        System.out.println("Visitor Name: "+firstVisitor.visitorName);
-        System.out.println("Visitor Contact Number: "+firstVisitor.phoneNumber);
+
+        Event event2 = new Event();
+        event2.eventName="Torch Light Parade";
+        event2.venue="Bannimantapa";
+        event2.eventDate="15-10-2025";
+        events.add(event2); //Push event2 object to events collection
+
+        Event event3 = new Event();
+        event3.eventName="Yuva Dasara";
+        event3.venue="Maharaja Grounds";
+        event3.eventDate="10-10-2025";
+        events.add(event3);
+
+        Event event4=new Event();
+        event4.eventName="Flower Show";
+        event4.venue="Palace Gate";
+        event4.eventDate="11-10-2025";
+        events.add(event4);
+
+        Event event5=new Event();
+        event5.eventName="Ahara Mela";
+        event5.venue="Lalitha Mahal Grounds";
+        event5.eventDate="12-10-2025";
+        events.add(event5);
+
+        //printing the collections of object in ArrayList
+        System.out.println("\n List of major events");
+        for(Event event:events){
+            System.out.println("-"+event.eventName);
+        }
     }
 }
