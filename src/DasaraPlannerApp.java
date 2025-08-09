b9487704a4c486fbf22fb72946f0a3c7c7dbed80
acreatorwithvision <fake49403@gmail.com>
2025-08-09 12:47:20 +0530

    Added user choice to make it interactive.

--- src/DasaraPlannerApp.java
+++ src/DasaraPlannerApp.java
@@ -1,10 +1,10 @@
 //Using ArrayList to store the collection of events.
 import java.util.ArrayList;
-
+import java.util.Scanner; //Scanner class for user input
 
 public class DasaraPlannerApp{
     public static void main(String[] args){
-        System.out.println("--- Mysuru Dasara 2025 Event Planner ---");
+        System.out.println("--- Welcome to Mysuru Dasara 2025 ---");
 
         //Initialize ArrayList for Event class to hold its objects as collections
         ArrayList<Event> events=new ArrayList<>();
@@ -41,10 +41,37 @@ public class DasaraPlannerApp{
         event5.eventDate="12-10-2025";
         events.add(event5);
 
-        //printing the collections of object in ArrayList
-        System.out.println("\n List of major events");
-        for(Event event:events){
-            System.out.println("-"+event.eventName);
+        //Scanner object creation to read input from user
+        Scanner scanner=new Scanner(System.in);
+        boolean isRunning=true;
+
+        //By default since isRunning is true, the program runs with the choice selector
+        while(isRunning) {
+            System.out.println("1. View all events");
+            System.out.println("2. Exit");
+            System.out.println("Enter your choice");
+
+
+            //Asking for an int input 1 or 2 from user
+            int choice = scanner.nextInt();
+            System.out.println("\n");
+            if (choice == 1) {
+                System.out.println("All the Dasara events this year\n");
+                for (Event event : events) {
+                    System.out.println("Event Name:" + event.eventName);
+                    System.out.println("Event Venue:" + event.venue);
+                    System.out.println("Event Date:" + event.eventDate);
+                    System.out.println("--------------");
+                }
+            }
+            else if(choice==2){
+                System.out.println("Thanks for using Dasara Planner, Goodbye");
+                isRunning=false;
+            }
+            else {
+                System.out.println("Invalid option, try again");
+            }
         }
+        scanner.close();
     }
 }
