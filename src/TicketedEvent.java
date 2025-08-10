5699f25b700effab88d793a2e9bb5d2b863a431d
acreatorwithvision <fake49403@gmail.com>
2025-08-10 14:05:21 +0530

    polymorhpic evolution of Event into Free and Ticket

---
+++ src/TicketedEvent.java
@@ -0,0 +1,15 @@
+public class TicketedEvent extends Event{
+    double ticketPrice;
+
+    //contructor for class TicketedEvent
+    public TicketedEvent(String eventName, String venue, String eventDate, EventType type){
+        super(eventname,venue,eventDate,type); //call parent contructor
+        this.ticketPrice=ticketPrice;
+    }
+
+    @Override //provied specific version of parent method
+    public void displayDetails(){
+        super.displayDetails();//call the parent method first and then add specific details
+        System.out.println(" Price: Rs."+this.ticketPrice);
+    }
+}
