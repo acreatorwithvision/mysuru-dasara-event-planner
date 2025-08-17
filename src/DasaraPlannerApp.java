1a40f9848a28022a58650640bc1e0dabd6598fe7
acreatorwithvision <fake49403@gmail.com>
2025-08-17 15:38:57 +0530

    created registration block logic with option to catch exceptions

--- src/DasaraPlannerApp.java
+++ src/DasaraPlannerApp.java
@@ -45,8 +45,36 @@ public class DasaraPlannerApp{
                 System.out.println("Enter your name");
                 String visitorName=scanner.nextLine();
 
+                //takes input for the event number to register to
                 System.out.println("Enter the Event # you want to register for:");
                 int eventNumber = scanner.nextInt();
+
+                //This block favors a condition where it checks the event to register is within the created rang
+                if (eventNumber>0 && eventNumber <=events.size()){
+                    Event selectedEvent=events.get(eventNumber-1);
+
+                    //Using try-with-resource helps write to a file making sure the writer is closed
+                    //after used once.
+                    //"true" flag sets the program to append to the file not overwrite it.
+
+                    try(FileWriter writer=new FileWriter("registrations.txt",true)){
+                        writer.write("Visitor: "+ visitorName +", Event: "+ selectedEvent.EventName +"\n");
+                        System.out.println("Registration complete..! Your details are saved.");
+                    }
+                    catch(IOException e){
+                        //this block will execute when the try block fails with an exception
+                        System.out.println("Error! Could not register to the event.");
+                        e.printStackTrace();
+                    }
+                }else{
+                    //If the choice exceeds out of the range, then this else executes
+                    System.out.println("Invalid Event number");
+
+                }
+            }
+            else if(choice==3){
+                isRunning=false;
+                System.out.println("Thanks for using Namma Mysuru Dasara Planner. GoodBye!")
             }
             else {
                 System.out.println("Invalid option, try again");
